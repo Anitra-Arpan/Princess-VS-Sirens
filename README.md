6dc0e845dae49c06ad002f287d961e433c73af27
Anitra-Arpan <60259235+Anitra-Arpan@users.noreply.github.com>
2020-05-15 20:56:55 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Princess-VS-Sirens
+# AngryBirdsStage3
+Stage 3 Angry Birds: Introducing Constraint
---
+++ Slingshot.js
@@ -0,0 +1,48 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.princess= loadImage('sprites/slingshot.png');
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    attach(body){
+        this.sling.bodyA = body;
+    }
+    
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+        image(this.princess,50,50,200,200);
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push();
+           /* 
+            stroke(48,22,8);
+            if(pointA.x < 220) {
+                strokeWeight(7);
+                line(pointA.x - 20, pointA.y, pointB.x -10, pointB.y);
+                line(pointA.x - 20, pointA.y, pointB.x + 30, pointB.y - 3);
+                image(this.sling3,pointA.x -30, pointA.y -10,15,30);
+            }
+            else{
+                strokeWeight(3);
+                line(pointA.x + 25, pointA.y, pointB.x -10, pointB.y);
+                line(pointA.x + 25, pointA.y, pointB.x + 30, pointB.y - 3);
+                image(this.sling3,pointA.x + 25, pointA.y -10,15,30);
+            }
+           
+           */ 
+            pop();
+        }
+    }
+    
+}
